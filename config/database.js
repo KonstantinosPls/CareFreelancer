2921b379279d69f33a3266af38544e567e9880a4
Argiris <anargpapous19@gmail.com>
2026-01-11 20:45:22 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
-    process.exit(1);
+    // process.exit(1); // Allow server to run without DB for UI preview
   }
 };
 
