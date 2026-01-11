dc45f4d2a0c69ff76f1a68653138dcdb0595a2ee
Argiris <anargpapous19@gmail.com>
2026-01-11 20:42:33 +0200

    Fix: HTML structure, ternary expression, deprecated MongoDB options

--- seed.js
+++ seed.js
@@ -7,10 +7,7 @@ const Gig = require('./models/Gig');
 const Order = require('./models/Order');
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
